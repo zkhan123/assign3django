dd0c62d864ad2255bb5b0fec8582deff62692531
Zoya <zmkhan@unomaha.edu>
2020-08-01 15:20:12 -0500

    initial commit

---
+++ efs_rest/urls.py
@@ -0,0 +1,36 @@
+"""efs_rest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token
+from portfolio import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', obtain_jwt_token),
+    path('', views.customer_list),
+    url(r'^api/customers/$', views.customer_list),
+    url(r'^api/customers/(?P<pk>[0-9]+)$', views.getCustomer),
+    path('investments/', views.investment_list),
+    url(r'^api/investments/$', views.investment_list),
+    url(r'^api/investments/(?P<pk>[0-9]+)$', views.getInvestment),
+    path('stocks/', views.stock_list),
+    url(r'^api/stocks/$', views.stock_list),
+    url(r'^api/stocks/(?P<pk>[0-9]+)$', views.getStock)
+
+]
